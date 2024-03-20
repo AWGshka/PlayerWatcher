096754367a413a31874cefd7e0b171a3def96e4c
Alex <thealexkit@gmail.com>
2024-03-20 20:19:37 +0100

    remove unneeded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { fetchPlayers, updateStatuses } from "./utils.js";
 
-const toFetch = [797691185, 1159754487, 961752903, 1165808661, 350283018, 487824426, 1161882879];
+const toFetch = [797691185, 1159754487];
 
 console.clear();
 console.log("[PlayerWatcher] Fetching players...");
